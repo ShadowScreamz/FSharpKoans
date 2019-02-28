5afa5edb59d9d05871a2f126a23660c0864dc5a4
Tshegofatso Molobe <G17M9848@ict.ru.ac.za>
2019-02-28 16:41:54 +0200

    Second commit: Changes made

--- FSharpKoans/AboutBinding.fs
+++ FSharpKoans/AboutBinding.fs
@@ -60,21 +60,21 @@ module ``02: About Binding`` =
     [<Test>]
     let ``01 Basic 'let' binding`` () = 
         let x = 50 in // note that the syntax is more explicit about what's really going on!
-            x |> should equal __
+            x |> should equal 50
 
     [<Test>]
-    let ``02 Equivalent basic 'let' binding`` () = // this is exactly equivalent to the previous binding.
+    let ``02 Equivalent basic 'let' binding`` () =  // this is exactly equivalent to the previous binding.
         let x = 50
-        x |> should equal __
+        x |> should equal 50
 
     [<Test>]
     let ``03 There are many types of values`` () =
-        let a = __
-        let b = __
-        let c = __
-        let d = __
-        let e = __
-        let f = __
+        let a = 1
+        let b = 1.1
+        let c = true
+        let d = "abc"
+        let e = 'a'
+        let f = ()
         a |> should be ofType<int>
         b |> should be ofType<float>
         c |> should be ofType<bool>
@@ -84,26 +84,26 @@ module ``02: About Binding`` =
 
     [<Test>]
     let ``04 We can compare values using F#'s comparison operators`` () =
-        1 |???| 2 |> should equal true
-        2 |???| 1 |> should equal true
-        1 |???| 1 |> should equal true
-        1 |???| 1 |> should equal false
-        () |???| () |> should equal true
-        () |???| () |> should equal false
+        1  <  2 |> should equal true
+        2  >  1 |> should equal true
+        1  =  1 |> should equal true
+        1  >  1 |> should equal false
+        ()  =  () |> should equal true
+        ()  >  () |> should equal false
 
     [<Test>]
     let ``05 There's a `not` function instead of a `not` operator`` () =
-        __ true |> should equal false
+        not true |> should equal false
 
     [<Test>]
     let ``06 Nest your 'let' statements as deeply as you'd like`` () =
         let a =
             let b =
                 let c =
-                    let d = 63 in d
+                    let d = 63 in d 
                 c + 1
             b + 7
-        a |> should equal ___
+        a |> should equal 71
 
 (*
     Identifiers are *referentially transparent*: the link between value and identifier never changes.
@@ -134,14 +134,14 @@ module ``02: About Binding`` =
     [<Test>]
     let ``07 Shadowing`` () =
         let a = 21
-        let b =
+        let b = 
             let a = 8
             3 + a
         let c = a + 4
         let a = a + a
-        a |> should equal __
-        b |> should equal __
-        c |> should equal __
+        a |> should equal 42
+        b |> should equal 11
+        c |> should equal 25
 
     (*
         The next test demonstrates *type inference*.
@@ -181,9 +181,9 @@ module ``02: About Binding`` =
 
     [<Test>]
     let ``08 An identifier pattern will match anything`` () =
-        let x = __ // replace with an integer
-        let y = __ // replace with a string
-        let z = __ // replace with anything else!
+        let x = 1 // replace with an integer
+        let y = "abc" // replace with a string
+        let z = 'a' // replace with anything else!
         x |> should be ofType<int>
         y |> should be ofType<string>
 
